e6f3ed0aa2ddb56fbed8b410c1f265e3471f0016
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2024-09-14 10:27:44 +0800

    next Link routes

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,21 @@
+import Link from "next/link";
+
 export default function ClientsPage() {
+  const clients = [
+    { id: "robin", name: "Ain Mutaqorrobin" },
+    { id: "yani", name: "Mariani" },
+    { id: "haikal", name: "Muhammad Haikal" },
+  ];
   return (
     <div>
       <h1>The Clients Page</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
