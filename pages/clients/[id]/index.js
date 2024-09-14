6b2212c72eb3b6dc5379bd19b02aa96bfda239a3
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2024-09-14 10:51:31 +0800

    navigating programmatically

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,12 +1,19 @@
 import { useRouter } from "next/router";
-
 export default function ClientProjectsPage() {
   const router = useRouter();
   console.log(router.query);
 
+  function loadProject() {
+    const client = { id: "robin", clientprojectid: "ExpressProject" };
+    console.log(`/clients/${client.id}/${client.clientprojectid}`);
+
+    router.push(`/clients/${client.id}/${client.clientprojectid}`);
+  }
+
   return (
     <div>
       <h1>The Projects of a Client</h1>
+      <button onClick={loadProject}>Load Project A</button>
     </div>
   );
 }
