a8c9b7256b77fc06294e6b822097a279a8e6be6e
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2024-09-17 15:09:20 +0800

    get year and month using catch all routes

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,4 +1,17 @@
+import { useRouter } from "next/router";
+import { getFilteredEvents } from "../../data/dummy-data";
+
 export default function FilteredEventsPage() {
+  const router = useRouter();
+  console.log(router.query);
+
+  const {
+    slug: [year, month],
+  } = router.query;
+
+  const filteredEvents = getFilteredEvents(year, month);
+  console.log(filteredEvents);
+
   return (
     <div>
       <h1>Filtered Events</h1>
